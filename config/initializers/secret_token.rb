250a74698a4fe6666b489271b06f8e3daf1c3284
Jonathan Levine <jonlevine@dhcp-172-22-87-64.mtv.corp.google.com>
2011-05-27 10:30:46 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eathckr::Application.config.secret_token = '67be1c3f8901227e90366d833652d9ee42835bfa24746912aae9733af2d2c7e1b10472259c1d9e4871c2bcf8f8735cd2431c9f5afed6e9b49934cb001ce6958d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Eathckr::Application.config.session_store :cookie_store, :key => '_eathckr_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Eathckr::Application.config.session_store :active_record_store
