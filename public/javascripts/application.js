99878541876d624ada6c077119202bf5aab8ac29
Jonathan Levine <jonlevine@google.com>
2011-06-01 00:27:19 -0700

    show or hide guest list

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+  $('.expand_guest_list').click(function() {
+    $('ol.guest_list').toggle();
+    if ($('ol.guest_list').css('display') == 'none') {
+      $(this).html('show guest list');
+    } else {
+      $(this).html('hide guest list');
+    }
+  });
+});
